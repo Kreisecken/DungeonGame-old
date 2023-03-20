5a65d0f9db420791676629bd0d657e8849a84f60
NextLegacy <mcserezli@gmail.com>
2023-03-20 23:48:14 +0100

    initialized csharp project

--- DungeonGame/Assets/Scripts/Testing/HelloWorld.cs
+++ DungeonGame/Assets/Scripts/Testing/HelloWorld.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+namespace DungeonGame 
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HelloWorld : MonoBehaviour
     {
-        
-    }
+        void Start()
+        {
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Hello, World!");
+        void Update()
+        {
+            Debug.Log("Hello World!");
+        }
     }
 }
