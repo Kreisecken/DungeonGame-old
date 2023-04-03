869338f493a62ac2455d834265d168de2ad8497f
NextLegacy <mcserezli@gmail.com>
2023-04-03 04:19:23 +0200

    began implementing procedural dungeon generator

---
+++ Assets/Scripts/Dungeon/Dungeon.cs
@@ -0,0 +1,39 @@
+﻿using DungeonGame.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonGame.Dungeon
+{
+    // rough prototyping
+
+    public class Dungeon : MonoBehaviour
+    {
+        public DungeonAsset asset;
+
+        public List<DungeonRoom> rooms;
+    
+        public void GenerateDungeon(DungeonAsset asset, Seed seed, GameObject parent)
+        {
+            // get rooms from asset
+            // -> keep in mind that some rooms are going to be locked
+            // -> theirfor some key elements could be out of reach
+            // -> after the following step, so this part needs to be rethought
+            // distribute them evenly
+            // create delaunay triangulation
+            // find minimum spanning tree
+            // choose random edges
+            // pathfind hallways
+            // finally place tiles and do final touches 
+        }
+    }
+
+    public class DungeonRoom : MonoBehaviour
+    {
+        // this class could handle DungeonRoom events
+        // like OnPlayerEnter() or OnPlayerMove()
+        // this could be usefull for example closing the doors
+        // when the player enters and opening them again
+        // after all monsters die
+    }
+}
---
+++ Assets/Scripts/Dungeon/DungeonAsset.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonGame.Dungeon
+{
+    // prototyping the DungeonGenerator
+    // the idea is the following:
+    // create a DungeonAsset with all the informations for generating a Dungeon
+    // to generate the Dungeon, we can pass a Generate() method the
+    // asset and a seed to generate a Dungeon
+    // giving the player the ability to use a seed
+    // would be kinda op, so maybe the player could
+    // spend money, clear the dungeon once or
+    // clear the dungeon once on the hardest difficulty?
+
+    [CreateAssetMenu(menuName = "DungeonGame/Dungeon Asset")]
+    public class DungeonAsset : ScriptableObject
+    {
+        public List<DungeonRoomAsset> rooms;
+    }
+
+    [Serializable]
+    public class DungeonRoomAsset
+    {
+        public DungeonRoom dungeonPrefab; // DungeonRoom or GameObject?
+
+        public int minCount;
+        public int maxCount;
+
+        [Range(0, 1)]
+        public float probability;
+
+        public List<DungeonRoomAsset> additionalRequiredRooms;
+    }
+}
