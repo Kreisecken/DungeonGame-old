a6b08dcc9ff523db66ad6fa85ca5083af24eae4f
NextLegacy <mcserezli@gmail.com>
2023-04-03 04:17:53 +0200

    implemented md5 hashcode

---
+++ Assets/Scripts/Utils/StringUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+namespace DungeonGame.Utils
+{
+    public static class StringUtils
+    {
+        private static MD5 MD5_Instance { get; } = MD5.Create();
+
+        // string.GetHashCode() returns different results
+        // for different devices, but this Methods does not
+        public static long MD5HashCode(this string str)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+
+            byte[] hash = MD5_Instance.ComputeHash(bytes);
+
+            long result = BitConverter.ToInt64(hash, 0);
+
+            return result;
+        }
+    }
+}
