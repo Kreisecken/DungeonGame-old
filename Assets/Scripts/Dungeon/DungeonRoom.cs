2a006cbed49482e20b18a585a38aa74f0ecc2993
NextLegacy <mcserezli@gmail.com>
2023-04-04 23:47:18 +0200

    moved DungeonRoom class out of Dungeon.cs

---
+++ Assets/Scripts/Dungeon/DungeonRoom.cs
@@ -0,0 +1,21 @@
+using DungeonGame.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace DungeonGame.Dungeon
+{
+    public class DungeonRoom : MonoBehaviour
+    {
+        public string roomName;
+        // OnPlayerEnter()
+        // OnAllEnemysGone()
+
+        // LockDoors()
+        // UnlockDoors()
+
+        // LockDoor(int doorIndex)
+        // UnlockDoor(int doorIndex()
+    }
+}
