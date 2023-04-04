c0ae3eefb8873579855b5c0c9b8e9e74b6f298b7
NextLegacy <mcserezli@gmail.com>
2023-04-04 23:49:52 +0200

    continued implementation of DungeonGeneration

--- Assets/Scripts/Dungeon/Dungeon.cs
+++ Assets/Scripts/Dungeon/Dungeon.cs
@@ -1,39 +1,85 @@
 ﻿using DungeonGame.Utils;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace DungeonGame.Dungeon
 {
-    // rough prototyping
-
     public class Dungeon : MonoBehaviour
     {
-        public DungeonAsset asset;
+        public DungeonConfiguration configuration;
 
-        public List<DungeonRoom> rooms;
-    
-        public void GenerateDungeon(DungeonAsset asset, Seed seed, GameObject parent)
+        public DungeonRoom startRoom;
+        public List<DungeonSection> sections;
+
+        public SeedableRandom random;
+
+        [ContextMenu("DungeonGenerator/TestGeneration")]
+        public void TestGeneration()
         {
-            // get rooms from asset
-            // -> keep in mind that some rooms are going to be locked
-            // -> theirfor some key elements could be out of reach
-            // -> after the following step, so this part needs to be rethought
-            // distribute them evenly
-            // create delaunay triangulation
-            // find minimum spanning tree
-            // choose random edges
-            // pathfind hallways
-            // finally place tiles and do final touches 
+            GenerateDungeon(configuration, "Hello, World!");
+        }
+
+        public void GenerateDungeon(DungeonConfiguration configuration, SeedableRandom random)
+        {
+            DestroyDungeon();
+        
+            this.configuration = configuration;
+            this.random        = random;
+
+            CreateRooms();
+            PlaceRoomsRandomly();
+            // LayOutRooms()
+            // DelaunayTriangulation()
+            // GenerateMinimumSpanningTree()
+            // AddSomeEdgesFromDelauneyBack(float percentage)
+            // GenerateHallways()
+            // Done() ?
+        }
+
+        public void CreateRooms()
+        {
+            foreach (var sectionConfiguration in configuration.sections)
+            {
+                GameObject gameObject = new(sectionConfiguration.dungeonSectionName);
+                DungeonSection section = gameObject.AddComponent<DungeonSection>();
+
+                section.transform.parent = transform;
+
+                foreach (var roomConfiguration in sectionConfiguration.rooms)
+                {
+                    roomConfiguration.CreateRooms(section.transform, section.Rooms, random);
+                }
+
+                sections.Add(section);
+            }
+        }
+
+        public void PlaceRoomsRandomly()
+        {
+            foreach (DungeonSection section in sections)
+            {
+                section.transform.position = random.PointInsideUnitCircle();
+            
+                foreach (DungeonRoom room in section.Rooms)
+                {
+                    room.transform.position = random.PointInsideUnitCircle();
+                }
+            }
+        }
+
+        public void DestroyDungeon()
+        {
+            sections = new List<DungeonSection>();
+
+            foreach (Transform child in transform)
+                GameObject.Destroy(child.gameObject);
         }
     }
 
-    public class DungeonRoom : MonoBehaviour
+    public class DungeonSection : MonoBehaviour
     {
-        // this class could handle DungeonRoom events
-        // like OnPlayerEnter() or OnPlayerMove()
-        // this could be usefull for example closing the doors
-        // when the player enters and opening them again
-        // after all monsters die
+        public List<DungeonRoom> Rooms { get; set; }
     }
 }
---
+++ Assets/Scripts/Dungeon/DungeonConfiguration.cs
@@ -0,0 +1,65 @@
+﻿using DungeonGame.Utils;
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonGame.Dungeon
+{
+    [Serializable, CreateAssetMenu(menuName = "DungeonGame/DungeonConfiguration")]
+    public class DungeonConfiguration : ScriptableObject
+    {
+        public string dungeonName;
+
+        public List<DungeonRoomConfiguration> independentRooms;
+
+        public List<DungeonSectionConfiguration> sections;
+    }
+
+    [Serializable]
+    public class DungeonRoomConfiguration
+    {
+        public DungeonRoom room;
+
+        public int minCount;
+        public int maxCount;
+
+        [Range(0, 1)]
+        public float probability;
+
+        public int minNeighbours;
+        public int maxNeighbours;
+
+        public bool isStartRoom;
+        public bool isSectorConnection;
+
+        public void CreateRooms(Transform parent, List<DungeonRoom> list, SeedableRandom random)
+        {
+            int count = minCount;
+
+            for (int i = 0; i < maxCount - minCount; i++)
+                if (random.Bool(probability))
+                    count++;
+
+            for (int i = 0; i < count; i++)
+            {
+                DungeonRoom dungeonRoom = DungeonRoom.Instantiate(room, parent);
+
+                dungeonRoom.name = dungeonRoom.roomName;
+
+                list.Add(dungeonRoom);
+            }
+        }
+    }
+
+    [Serializable]
+    public class DungeonSectionConfiguration
+    {
+        public string dungeonSectionName;
+
+        // maybe too much, but maybe define for this min, max and probability?
+
+        public List<DungeonRoomConfiguration> rooms;
+    }
+}
