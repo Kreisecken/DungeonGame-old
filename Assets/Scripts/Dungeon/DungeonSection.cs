ece8cd2c03091ab1ac37c55b07e6a7ae74e583be
NextLegacy <mcserezli@gmail.com>
2023-04-08 23:11:30 +0200

    continued implementing dungeon generation

--- Assets/Scripts/Dungeon/DungeonSection.cs
+++ Assets/Scripts/Dungeon/DungeonSection.cs
@@ -1,5 +1,5 @@
 ﻿using DungeonGame.Dungeon;
-using System.Collections;
+using DungeonGame.Utils;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,63 @@ namespace DungeonGame.Dungeon
 {
     public class DungeonSection : MonoBehaviour
     {
+        public Dungeon dungeon;
+        public DungeonSectionConfiguration sectionConfig;
+        public SeedableRandom random;
+
         public List<DungeonRoom> rooms;
+
+
+        public void Generate(Dungeon dungeon, List<DungeonRoom> independentRooms, DungeonSectionConfiguration sectionConfig)
+        {
+            random = dungeon.random;
+            this.sectionConfig = sectionConfig;
+
+            dungeon.CreateRooms(sectionConfig.rooms, ref rooms);
+
+            PlaceRoomsRandomly();
+            UseIndependentRooms(independentRooms);
+            dungeon.CreateRooms(sectionConfig.rooms, ref rooms);
+
+            SetPositionsOfRooms();
+        }
+
+        public void PlaceRoomsRandomly()
+        {
+            foreach (DungeonRoom room in rooms)
+                room.transform.position = random.PointInsideUnitCircle();
+        }
+
+        public void UseIndependentRooms(List<DungeonRoom> independentRooms)
+        {
+            int count = random.Int32(independentRooms.Count);
+
+            for (int i = 0; i < count; i++)
+            {
+                rooms.Add(independentRooms[i]);
+                independentRooms.RemoveAt(i);
+            }
+        }
+
+        public void SetPositionsOfRooms()
+        {
+            rooms.Shuffle(random);
+
+            /*
+             * Pseudo Code
+             * 
+             * list rooms;
+             * 
+             * foreach (var room in rooms)
+             * {
+             *     room.done = true;
+             *     
+             *     while (rooms collide with rooms[0])
+             *     {
+             *         move colliding rooms until all collisions are resolved
+             *     }
+             * }
+             */
+        }
     }
 }
