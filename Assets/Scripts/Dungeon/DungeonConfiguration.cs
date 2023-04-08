89f4b01b8d4cacbc4d857b9720841be3584814f1
NextLegacy <mcserezli@gmail.com>
2023-04-08 23:06:18 +0200

    simplified configurations

--- Assets/Scripts/Dungeon/DungeonConfiguration.cs
+++ Assets/Scripts/Dungeon/DungeonConfiguration.cs
@@ -28,37 +28,15 @@ namespace DungeonGame.Dungeon
         [Range(0, 1)]
         public float probability;
 
-        public int minNeighbours;
-        public int maxNeighbours;
-
         public bool isStartRoom;
         public bool isSectorConnection;
-
-        public void CreateRooms(Transform parent, List<DungeonRoom> list, SeedableRandom random)
-        {
-            int count = minCount;
-
-            for (int i = 0; i < maxCount - minCount; i++)
-                if (random.Bool(probability))
-                    count++;
-
-            for (int i = 0; i < count; i++)
-            {
-                DungeonRoom dungeonRoom = DungeonRoom.Instantiate(room, parent);
-
-                dungeonRoom.name = dungeonRoom.roomName;
-
-                list.Add(dungeonRoom);
-            }
-        }
     }
 
     [Serializable]
     public class DungeonSectionConfiguration
     {
-        public string dungeonSectionName;
-
         // maybe too much, but maybe define for this min, max and probability?
+        public string dungeonSectionName;
 
         public List<DungeonRoomConfiguration> rooms;
     }
