a52fe644b2b67855722395aa1a2803dca2ed30fe
El_Tigere <eltigere@web.de>
2023-05-13 19:00:16 +0200

    players can hold weapons

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -8,12 +8,23 @@ namespace DungeonGame.Player
     public class PlayerScript : MonoBehaviour
     {
         [Min(0)] public int maxHealth;
+        public Transform weaponDirection;
+        public WeaponType testWeaponTypeBecauseWeaponsCanNotBeCollectedYet;
         
         private int health;
+        private Weapon weapon;
         
         void Start()
         {
             health = maxHealth;
+            
+            // TODO: collect weapons instead of just having them
+            //weapon = new Weapon(testWeaponTypeBecauseWeaponsCanNotBeCollectedYet, weaponDirection);
+        }
+        
+        void FixedUpdate()
+        {
+            if(weapon != null) weapon.WeaponUpdate(Time.fixedDeltaTime);
         }
         
         public void Damage(int amount, DamageType damageType)
@@ -22,6 +33,16 @@ namespace DungeonGame.Player
             health -= amount;
             
             if(health <= 0) Destroy(gameObject);
+        }
+        
+        public void SetWeaponTrigger(bool trigger)
+        {
+            if(weapon != null) weapon.trigger = trigger;
+        }
+        
+        public void SetWeaponDirection(Vector2 direction)
+        {
+            weaponDirection.rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
         }
     }
 }
