da2c52b2cf3bc3abb400d019c87854496a159aad
NextLegacy <mcserezli@gmail.com>
2023-06-03 15:36:39 +0200

    refactored Edge

--- Assets/Scripts/Utils/Graph/Edge.cs
+++ Assets/Scripts/Utils/Graph/Edge.cs
@@ -3,28 +3,36 @@ using System.Collections;
 using UnityEngine;
 using static UnityEngine.Rendering.DebugUI;
 using System;
+using Unity.VisualScripting;
 
 namespace DungeonGame.Utils.Graph
 {
     [Serializable]
-    public struct Edge<T>
+    public class Edge<T>
     {
         public Vertex<T> a;
         public Vertex<T> b;
 
         public float weight;
 
-        public Edge(Vertex<T> a, Vertex<T> b, float weight = 0.0f)
+        public Edge(Vertex<T> a, Vertex<T> b, float weight)
         {
             this.a = a;
             this.b = b;
 
+            if (a == b) throw new Exception("Vertecies are equal!");
+
             this.weight = weight;
+        }
+        
+        public bool HasVertex(Vertex<T> vertex)
+        {
+            return a == vertex || b == vertex;
         }
 
         public override string ToString()
         {
-            return $"e({a}, {b})";
+            return $"Edge({a}, {b})";
         }
 
         public override bool Equals(object obj)
