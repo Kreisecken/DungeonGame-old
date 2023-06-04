adba39e862585d66ef62f4f5702d565e0f48baa7
NextLegacy <mcserezli@gmail.com>
2023-06-04 15:38:35 +0200

    added GetAndRemoveRandom()

--- Assets/Scripts/Utils/ListUtils.cs
+++ Assets/Scripts/Utils/ListUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,9 +14,21 @@ namespace DungeonGame.Utils
 
         public static T GetRandom<T>(this IList<T> list, SeedableRandom random)
         {
-            if (list.Count == 0) throw new System.Exception("list size is zero");
+            if (list.Count == 0) return default;
 
             return list[random.Int32(0, list.Count - 1)];
+        }
+
+        public static T GetAndRemoveRandom<T>(this IList<T> list, SeedableRandom random)
+        {
+            if (list.Count == 0) return default;
+
+            int index = random.Int32(0, list.Count - 1);
+
+            T value = list[index];
+            list.RemoveAt(index);
+
+            return value;
         }
     }
 }
