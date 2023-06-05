3592b49d200212bb4d27ecd5b098ec7b674d7f65
NextLegacy <mcserezli@gmail.com>
2023-06-05 15:50:42 +0200

    stored neighbours in a different way

--- Assets/Scripts/Dungeon/DungeonRoom.cs
+++ Assets/Scripts/Dungeon/DungeonRoom.cs
@@ -19,8 +19,8 @@ namespace DungeonGame.Dungeon
         public DungeonConfiguration config;
         public Dungeon dungeon;
 
-        //public List<DungeonRoom> neighbours;
-        public DungeonRoomConnections connections;
+        public DungeonRoomConnections relations;
+        public List<DungeonRoom> Neighbours => relations.neighbours;
 
         public Triangle<DungeonRoom> triangle;
 
@@ -32,6 +32,7 @@ namespace DungeonGame.Dungeon
         {
             this.dungeon = dungeon;
             this.config = dungeon.config;
+            relations = new(this);
         }
 
         public bool Intersects(DungeonRoom room)
@@ -52,7 +53,7 @@ namespace DungeonGame.Dungeon
 
         private void OnDrawGizmos()
         {
-            foreach (var neighbour in neighbours)
+            foreach (var neighbour in Neighbours)
             {
                 Gizmos.DrawLine(transform.position, neighbour.transform.position);
             }
@@ -65,7 +66,7 @@ namespace DungeonGame.Dungeon
             Gizmos.color = Color.blue;
             Gizmos.DrawWireSphere(triangle.circumCircle.center, triangle.circumCircle.radius);
 
-            foreach (var neighbour in neighbours)
+            foreach (var neighbour in Neighbours)
             {
                 Gizmos.color = Color.red;
 
@@ -75,7 +76,19 @@ namespace DungeonGame.Dungeon
                 Gizmos.DrawLine(a, neighbour.transform.position);
             }
 
+            foreach (var connection in relations.connections)
+            {
+                Gizmos.color = Color.green;
 
+                Gizmos.DrawLine(connection.AConnectionPoint, connection.BConnectionPoint);
+
+                Gizmos.color = Color.yellow;
+                
+                Gizmos.DrawCube(connection.AConnectionPoint, new Vector3(1, 1, 1));
+                Gizmos.DrawCube(connection.BConnectionPoint, new Vector3(1, 1, 1));
+            }
+
+            /*
             for (float i = 0; i < 360; i += 2.5f)
             {
                 Vector3 directionRotated = Quaternion.Euler(0, 0, i) * Vector2.one;
@@ -86,19 +99,19 @@ namespace DungeonGame.Dungeon
 
                 var hit = hits.OrderBy((hit) => hit.distance).ElementAt(1);
 
-
                 if (hit.collider != null)
                 {
-                    if (!neighbours.Contains(hit.collider.gameObject.GetComponent<DungeonRoom>())) continue;
+                    if (!Neighbours.Contains(hit.collider.gameObject.GetComponent<DungeonRoom>())) continue;
 
                     Gizmos.DrawLine(transform.position, transform.position + directionRotated * hit.distance*0.7f);
                     Gizmos.DrawCube(new(hit.point.x, hit.point.y, 0), new(1, 1, 1)); 
                 }
-            }
+            }*/
         }
     }
 
-    public struct DungeonRoomConnections
+    [Serializable]
+    public class DungeonRoomConnections
     {
         public DungeonRoom room;
 
@@ -106,21 +119,49 @@ namespace DungeonGame.Dungeon
 
         public List<DungeonRoomConnection> connections;
 
+        public DungeonRoomConnections(DungeonRoom room)
+        {
+            this.room = room;
+            this.neighbours = new();
+            this.connections = new();
+        }
+
         public void Add(DungeonRoom neighbour)
         {
             if (room == neighbour) return;
             if (neighbours.Contains(neighbour)) return;
 
-            neighbour.dungeonRoomConnections        
+            DungeonRoomConnection connection = new(room, neighbour, Vector3.zero, Vector3.zero);
+
+            connections.Add(connection);
+
+            neighbours.Add(neighbour);
+
+            neighbour.relations.neighbours.Add(room);
+            neighbour.relations.connections.Add(connection);
+        }
+
+        public DungeonRoomConnection GetConnection(DungeonRoom neighbour)
+        {
+            return connections.Find((connection) => connection.A == neighbour || connection.B == neighbour);
         }
     }
 
-    public struct DungeonRoomConnection
+    [Serializable]
+    public class DungeonRoomConnection
     {
         public DungeonRoom A;
         public DungeonRoom B;
 
-        public Vector3 tileA;
-        public Vector3 tileB;
+        public Vector3 AConnectionPoint;
+        public Vector3 BConnectionPoint;
+
+        public DungeonRoomConnection(DungeonRoom a, DungeonRoom b, Vector3 tileA, Vector3 tileB)
+        {
+            A = a;
+            B = b;
+            this.AConnectionPoint = tileA;
+            this.BConnectionPoint = tileB;
+        }
     }
 }
