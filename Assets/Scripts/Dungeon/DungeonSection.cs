27d7536a290516d45e78a03b0308caa307c35859
NextLegacy <mcserezli@gmail.com>
2023-06-05 15:51:27 +0200

    added MergeTileMaps() and CreateConnectionPoints()

--- Assets/Scripts/Dungeon/DungeonSection.cs
+++ Assets/Scripts/Dungeon/DungeonSection.cs
@@ -9,6 +9,7 @@ using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.Tilemaps;
 
 namespace DungeonGame.Dungeon
 {
@@ -21,7 +22,9 @@ namespace DungeonGame.Dungeon
         public List<DungeonRoom> rooms;
 
         [SerializeField]
-        public Graph<DungeonRoom> graph;
+        public Graph3<DungeonRoom> graph;
+
+        public Tilemap tileMap;
 
         [ContextMenu("Update")]
         public void Refresh()
@@ -44,6 +47,7 @@ namespace DungeonGame.Dungeon
             {
                 Vector3 floored = new ((int) room.transform.position.x, (int) room.transform.position.y);
                 room.transform.position = floored;
+                
                 graph.AddVertex(new(room, floored));
             }
 
@@ -51,6 +55,10 @@ namespace DungeonGame.Dungeon
             RemoveSomeEdges();
 
             ApplyGraphToRooms();
+
+            MergeTileMaps();
+
+            CreateConnectionPoints();
         }
 
         public void Init(Dungeon dungeon, DungeonSectionConfiguration sectionConfig)
@@ -140,7 +148,7 @@ namespace DungeonGame.Dungeon
 
         public void Triangulate()
         {
-            graph.Triangulate();
+            Triangulation.Triangulate(graph);
         }
     
         public void RemoveSomeEdges()
@@ -150,7 +158,7 @@ namespace DungeonGame.Dungeon
                 edge.weight = (edge.a.position - edge.b.position).magnitude;
             }
 
-            List<Edge<DungeonRoom>> removedEdges = graph.MinimumSpanningTree().ToList();
+            List<Edge3<DungeonRoom>> removedEdges = graph.MinimumSpanningTree().ToList();
 
             for (int i = 0, count = (int)(random.Float32() * sectionConfig.cycles * removedEdges.Count); i < count; i++)
                 graph.AddEdge(removedEdges.GetAndRemoveRandom(random));
@@ -162,9 +170,119 @@ namespace DungeonGame.Dungeon
             {
                 foreach (var edge in edges)
                 {
-                    vertex.data.neighbours.Add(edge.a.data);
-                    vertex.data.neighbours.Add(edge.b.data);
+                    vertex.data.relations.Add(vertex.data == edge.a.data ? edge.b.data : edge.a.data);
                 }
+            }
+        }
+    
+        public void MergeTileMaps()
+        {
+            GameObject tileMapGameObject = Instantiate(dungeon.config.tileMapPrefab, transform);
+
+            tileMap = tileMapGameObject.GetComponentInChildren<Tilemap>();
+
+            foreach (var dungeonRoom in rooms)
+            {
+                foreach (var position in dungeonRoom.tileMap.cellBounds.allPositionsWithin)
+                {
+                    if (!dungeonRoom.tileMap.HasTile(position)) continue;
+
+                    Vector3 translatedPosition = position + dungeonRoom.transform.position;
+                    Debug.Log(dungeonRoom.tileMap.GetTile(position));
+                    tileMap.SetTile(new Vector3Int((int) translatedPosition.x, (int) translatedPosition.y, 0), dungeonRoom.tileMap.GetTile(position));
+                }
+
+                // TODO: change to Destroy()
+
+                DestroyImmediate(dungeonRoom.tileMap.gameObject.transform.parent.gameObject);
+
+                dungeonRoom.tileMap = tileMap;
+            }
+        }
+ 
+        public void CreateConnectionPoints()
+        {
+            var remainingRooms = rooms.ToList();
+
+            while (remainingRooms.Count() > 0)
+            {
+                var room = remainingRooms[0];
+
+                Dictionary<DungeonRoom, List<Vector3>> neighbourHits = new();
+
+                for (float i = 0; i < 360; i += 1)
+                {
+                    Vector3 directionRotated = Quaternion.Euler(0, 0, i) * Vector2.one;
+
+                    RaycastHit2D[] rayCastHits = Physics2D.RaycastAll(room.transform.position, directionRotated);
+
+                    if (rayCastHits.Length < 2) continue;
+
+                    var hits = rayCastHits.Where((hit) => 
+                        hit.collider != room.collider2d && 
+                        room.Neighbours.Contains(hit.collider.gameObject.GetComponent<DungeonRoom>())
+                    );
+
+                    Debug.Log(hits.Count());
+
+                    if (hits.Count() == 0) continue;
+
+                    var hit = hits.ElementAt(0);
+
+                    if (!hit.collider.TryGetComponent(out DungeonRoom neighbour)) continue;
+
+                    if (!neighbourHits.ContainsKey(neighbour))
+                        neighbourHits.Add(neighbour, new());
+
+                    neighbourHits[neighbour].Add(hit.point);
+                }
+
+                foreach (var (neighbour, hits) in neighbourHits)
+                {
+                    Vector3 neighbourHitPoint = hits.GetRandom(random);
+                    
+                    List<Vector3> roomHits = new();
+
+                    for (float i = 0; i < 360; i += 1)
+                    {
+                        Vector3 directionRotated = Quaternion.Euler(0, 0, i) * Vector2.one;
+
+                        RaycastHit2D[] rayCastHits = Physics2D.RaycastAll(neighbourHitPoint, directionRotated);
+
+                        if (rayCastHits.Length < 2) continue;
+
+                        var rayCastHitsWithRoom = rayCastHits.Where((hit) => hit.collider == room.collider2d);
+
+                        Debug.Log(rayCastHitsWithRoom.Count() + "abc");
+
+                        if (rayCastHitsWithRoom.Count() == 0) continue;
+
+                        roomHits.Add(rayCastHitsWithRoom.ElementAt(0).point);
+                    }
+
+                    Vector3 roomHitPoint = roomHits.GetRandom(random);
+
+                    Debug.Log(roomHitPoint);
+                    Debug.Log(room.tileMap.WorldToCell(roomHitPoint));
+
+                    var connection = room.relations.connections.Find((connection) => connection.A == neighbour || connection.B == neighbour);
+
+                    var roundedRoomHitPoint = new Vector3Int((int) Mathf.Round(roomHitPoint.x), (int) Mathf.Round(roomHitPoint.y), 0);
+                    var roundedNeighbourHitPoint = new Vector3Int((int) Mathf.Round(neighbourHitPoint.x), (int) Mathf.Round(neighbourHitPoint.y), 0);
+
+                    if (connection.A == room)
+                    {
+                        connection.AConnectionPoint = roomHitPoint;
+                        connection.BConnectionPoint = neighbourHitPoint;
+                    }
+                    else
+                    {
+                        connection.AConnectionPoint = roomHitPoint;
+                        connection.BConnectionPoint = neighbourHitPoint;
+                    }
+                }
+
+                remainingRooms.RemoveAt(0);
             }
         }
     }
