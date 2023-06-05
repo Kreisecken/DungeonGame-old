ed4d23de57a35fcb223a34b4ad41b73436871072
NextLegacy <mcserezli@gmail.com>
2023-06-05 15:53:05 +0200

    GetRandom() now works on any IEnumerable<T>

--- Assets/Scripts/Utils/ListUtils.cs
+++ Assets/Scripts/Utils/ListUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace DungeonGame.Utils
@@ -10,13 +11,6 @@ namespace DungeonGame.Utils
         public static void Shuffle<T>(this IList<T> list, SeedableRandom random)
         {
             random.Shuffle(list);
-        }
-
-        public static T GetRandom<T>(this IList<T> list, SeedableRandom random)
-        {
-            if (list.Count == 0) return default;
-
-            return list[random.Int32(0, list.Count - 1)];
         }
 
         public static T GetAndRemoveRandom<T>(this IList<T> list, SeedableRandom random)
@@ -29,6 +23,13 @@ namespace DungeonGame.Utils
             list.RemoveAt(index);
 
             return value;
+        }
+
+        public static T GetRandom<T>(this IEnumerable<T> enumerable, SeedableRandom random)
+        {
+            if (enumerable.Count() == 0) return default;
+
+            return enumerable.ElementAt(random.Int32(0, enumerable.Count() - 1));
         }
     }
 }
