7feabdd91f87819464faf52777fb8990970252c0
NextLegacy <mcserezli@gmail.com>
2023-06-06 15:55:49 +0200

    refactored Vertex class

--- Assets/Scripts/Utils/Graph/Vertex.cs
+++ Assets/Scripts/Utils/Graph/Vertex.cs
@@ -14,26 +14,39 @@ namespace DungeonGame.Utils.Graph
     public class Vertex<T>
     {
         public readonly T data;
-        public readonly Vector2 position;
 
-        public Vertex(T data, Vector2 position)
+        public Vertex(T data)
         {
             this.data = data;
-            this.position = position;
         }
 
         public override string ToString()
         {
-            return $"Vertex(data: ${data}, position: ${position})";
+            return $"Vertex(data: ${data})";
         }
 
-        public bool EqualsVertex(Vertex<T> vertex) // TODO: does not feel right
+        public virtual bool EqualsVertex(Vertex<T> vertex)
         {
-            return vertex != null && data != null && data.Equals(vertex.data) && position.Equals(vertex.position);
+            return vertex is Vertex<T> vertex1 && data.Equals(vertex1.data);
         }
 
         public override int GetHashCode() => base.GetHashCode();
 
         public static implicit operator T(Vertex<T> vertex) => vertex.data;
+    }
+
+    public class Vertex3<T> : Vertex<T>
+    {
+        public readonly Vector3 position;
+
+        public Vertex3(T data, Vector3 position) : base(data)
+        {
+            this.position = position;
+        }
+
+        public override bool EqualsVertex(Vertex<T> vertex)
+        {
+            return base.EqualsVertex(vertex) && (vertex is not Vertex3<T> vertex3 || position.Equals(vertex3.position));
+        }
     }
 }
