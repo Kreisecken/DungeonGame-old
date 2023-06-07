47ff5771cd0b578803c5390f09fbb83d7494dce4
NextLegacy <mcserezli@gmail.com>
2023-06-07 16:16:29 +0200

    Dungeon Prefab class

---
+++ Assets/Scripts/Dungeon/DungeonPrefab.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace DungeonGame.Dungeons
+{
+    public class DungeonPrefab : MonoBehaviour
+    {
+        public Tilemap tileMap;
+
+        public Transform roomsContainerGameObject;
+    }
+}
