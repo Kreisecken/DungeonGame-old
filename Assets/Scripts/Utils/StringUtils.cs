0ee9822bcb48e5d377a9cee0bddc8b224f527065
NextLegacy <mcserezli@gmail.com>
2023-06-09 16:26:37 +0200

    added some char arrays

--- Assets/Scripts/Utils/StringUtils.cs
+++ Assets/Scripts/Utils/StringUtils.cs
@@ -8,10 +8,14 @@ namespace DungeonGame.Utils
 {
     public static class StringUtils
     {
+        public static char[] Alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static char[] Digits = "0123456789".ToCharArray();
+        public static char[] AlphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray();
+
         private static MD5 MD5_Instance { get; } = MD5.Create();
 
         // string.GetHashCode() returns different results
-        // for different devices, but this Methods does not
+        // for different devices, but this Method is (should be) consistent
         public static long MD5HashCode(this string str)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(str);
