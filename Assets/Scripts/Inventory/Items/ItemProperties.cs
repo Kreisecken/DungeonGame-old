504d1d1fab3bb49ac1ef71fa0d6b1b7fd9c68193
El_Tigere <eltigere@web.de>
2023-07-13 14:18:21 +0200

    changed asset menu of ItemProperties

--- Assets/Scripts/Inventory/Items/ItemProperties.cs
+++ Assets/Scripts/Inventory/Items/ItemProperties.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="NewItem", menuName ="Item/Create New Item")]
+[CreateAssetMenu(fileName ="NewItem", menuName ="DungeonGame/Item")]
 public class ItemProperties : ScriptableObject
 {
     public int id;
